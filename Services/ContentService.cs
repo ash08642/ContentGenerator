59379b07a03bb169cd1d9957b9604bcff4772616
ash08642 <108698229+ash08642@users.noreply.github.com>
2025-02-16 13:22:42 +0100

    implemented basic CRUD Operations with services

---
+++ Services/ContentService.cs
@@ -0,0 +1,44 @@
+using ContentGenerator.Models;
+
+namespace ContentGenerator.Services;
+
+public class ContentService
+{
+    private static readonly List<Content> AllContents = [];
+
+    public Task CreateContent(Content content) {
+        AllContents.Add(content);
+        return Task.CompletedTask;
+    }
+
+    public Task<Content?> UpdateContent(int id, Content content)
+    {
+        Content content1 = AllContents.FirstOrDefault(x => x.Id == id);
+        if (content1 != null)
+        {
+            content1.Text = content.Text;
+            content1.AudioPath = content.AudioPath;
+        }
+        return Task.FromResult(content1);
+    }
+
+    public Task<Content?> GetPost(int id) 
+    {
+        return Task.FromResult(AllContents.FirstOrDefault(x => x.Id == id));
+    }
+
+    public Task<List<Content>> GetAllContents()
+    {
+        return Task.FromResult(AllContents);
+    }
+
+    public Task DeleteContent(int id)
+    {
+        Content content = AllContents.FirstOrDefault(x => x.Id == id);
+        if (content != null)
+        {
+            AllContents.Remove(content);
+        }
+        return Task.CompletedTask;
+    }
+}
